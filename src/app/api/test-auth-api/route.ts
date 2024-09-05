33087a464b0527470886bd26ec3cb0e1069c4979
Alessandro Cipolletti <cipolletti.alessandro@gmail.com>
2024-09-06 01:40:58 +0200

    Chore: test logger

--- src/app/api/test-auth-api/route.ts
+++ src/app/api/test-auth-api/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from 'next/server'
+import logger from '@/backend/utils/logger'
 
 export async function POST(request: Request) {
+  logger.info({ data: 'value' }, 'api call test-auth-api')
   return NextResponse.json({})
 }

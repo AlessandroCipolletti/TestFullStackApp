cc1adfd1fb7e39c31bd155c1cf512c5197b0c44f
Alessandro Cipolletti <cipolletti.alessandro@gmail.com>
2024-09-21 17:27:54 +0200

    Feat: PageWrapper component

---
+++ src/frontend/components/PageWrapper.tsx
@@ -0,0 +1,10 @@
+import { styled } from '@mui/material/styles'
+
+const PageWrapper = styled('main')({
+  position: 'relative',
+  overflow: 'hidden',
+  margin: '32px auto 0px',
+  maxWidth: 1200,
+})
+
+export default PageWrapper

89742ed6183bec2c60058af5c37f4d1b4d44b81e
Alessandro Cipolletti <cipolletti.alessandro@gmail.com>
2024-10-10 23:30:36 +0200

    Using @mui/x-charts-pro

--- src/frontend/pages/dashboard/pages/ChartsPage.tsx
+++ src/frontend/pages/dashboard/pages/ChartsPage.tsx
@@ -1,44 +1,33 @@
 'use client'
 import React from 'react'
 import { Typography } from '@mui/material'
-import {
-  ResponsiveContainer,
-  LineChart,
-  Line,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-} from 'recharts'
-
-const data = [
-  { nome: 'Gen', uv: 4000, pv: 2400, amt: 2400 },
-  { nome: 'Feb', uv: 3000, pv: 1398, amt: 2210 },
-  { nome: 'Mar', uv: 2000, pv: 9800, amt: 2290 },
-  { nome: 'Apr', uv: 2780, pv: 3908, amt: 2000 },
-  { nome: 'Mag', uv: 1890, pv: 4800, amt: 2181 },
-  { nome: 'Giu', uv: 2390, pv: 3800, amt: 2500 },
-  { nome: 'Lug', uv: 3490, pv: 4300, amt: 2100 },
-]
+import { LineChart } from '@mui/x-charts/LineChart'
 
 export default function ChartsPage() {
+  const dataset = [
+    { x: 1, y: 2 },
+    { x: 2, y: 5.5 },
+    { x: 3, y: 2 },
+    { x: 5, y: 8.5 },
+    { x: 8, y: 1.5 },
+    { x: 10, y: 5 },
+  ]
+
   return (
     <div>
       <Typography variant="h4" gutterBottom>
         Grafici
       </Typography>
-      <ResponsiveContainer width="100%" height={400}>
-        <LineChart data={data}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="nome" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="pv" stroke="#8884d8" name="PV" />
-          <Line type="monotone" dataKey="uv" stroke="#82ca9d" name="UV" />
-        </LineChart>
-      </ResponsiveContainer>
+      <div style={{ height: 500 }}>
+        <LineChart
+          dataset={dataset}
+          xAxis={[{ dataKey: 'x' }]}
+          series={[{ dataKey: 'y' }]}
+          margin={{ left: 30, right: 30, top: 30, bottom: 30 }}
+          grid={{ vertical: true, horizontal: true }}
+          tooltip={{}}
+        />
+      </div>
     </div>
   )
 }
